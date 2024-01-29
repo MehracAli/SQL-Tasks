369d4bcd7f2d53664edba8bd01f9e830b9c65cd0
Mehraj Mammadov <mehrajvm@code.edu.az>
2024-01-29 05:39:02 +0400

    AddCategory done

---
+++ HW-2.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE AddCategory
+    @category NVARCHAR(255)
+AS
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM Categories WHERE CategoryName = @category)
+		BEGIN
+			INSERT INTO Categories (CategoryName)
+			VALUES (@category)
+
+			PRINT 'Category added'
+		END
+    ELSE
+		BEGIN
+			PRINT 'This category already have'
+		END
+END
+
+EXEC AddCategory 'NewCategory'
