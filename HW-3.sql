cd110e3d3f6e31b1214069b4426454294e39c8e1
Mehraj Mammadov <mehrajvm@code.edu.az>
2024-01-29 07:31:58 +0400

    2 task done

---
+++ HW-3.sql
@@ -0,0 +1,29 @@
+﻿CREATE PROCEDURE AddProduct
+    @CategoryName NVARCHAR(50),
+    @Description NVARCHAR(255),
+    @ProductName NVARCHAR(100),
+    @UnitPrice DECIMAL(10, 2),
+    @UnitsInStock INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @CategoryId INT;
+
+    IF NOT EXISTS (SELECT 1 FROM Categories WHERE CategoryName = @CategoryName)
+    BEGIN
+        INSERT INTO Categories (CategoryName, Description)
+        VALUES (@CategoryName, @Description);
+
+        SET @CategoryId = SCOPE_IDENTITY();
+    END
+    ELSE
+    BEGIN
+        SET @CategoryId = (SELECT CategoryID FROM Categories WHERE CategoryName = @CategoryName);
+    END
+
+    INSERT INTO Products (ProductName, CategoryID, UnitPrice, UnitsInStock)
+    VALUES (@ProductName, @CategoryId, @UnitPrice, @UnitsInStock);
+END;
+
+EXEC AddProduct 'NewCategory', 'Desc', 'Product', 100, 10;
---
+++ HW-5.sql
@@ -0,0 +1,20 @@
+﻿CREATE TRIGGER ShippersTrigger
+ON Shippers
+AFTER INSERT, UPDATE, DELETE
+AS
+BEGIN
+    IF EXISTS (SELECT 1 FROM inserted) AND NOT EXISTS (SELECT 1 FROM deleted) 
+    BEGIN
+        PRINT 'Save operation';
+    END;
+
+    IF EXISTS (SELECT 1 FROM inserted) AND EXISTS (SELECT 1 FROM deleted) 
+    BEGIN
+        PRINT 'Update operation';
+    END;
+
+    IF NOT EXISTS (SELECT 1 FROM inserted) AND EXISTS (SELECT 1 FROM deleted) 
+    BEGIN
+        PRINT 'Delete operation';
+    END;
+END;
